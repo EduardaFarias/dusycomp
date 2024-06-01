ae03e9330d18500bf392d4fea78190e828b19573
EduardaFarias <maria.eduarda.farias@ccc.ufcg.edu.br>
2024-06-01 15:15:04 -0300

    feat: add page title

--- dusycomp/front/src/App.js
+++ dusycomp/front/src/App.js
@@ -3,6 +3,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <div className="title">
+          Dusycomp
+        </div>
       </header>
     </div>
   );
