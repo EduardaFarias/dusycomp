82f79af839158ace12c7e46ed13ef7791d4e6508
EduardaFarias <maria.eduarda.farias@ccc.ufcg.edu.br>
2024-06-01 20:24:50 -0300

    feat: add text description

--- dusycomp/front/src/App.js
+++ dusycomp/front/src/App.js
@@ -8,6 +8,9 @@ function App() {
         <div className="title">
           Dusycomp
         </div>
+        <div className="text-description">
+            O Dusycomp é um site para armazenar e organizar minhas anotações e matereiais produzidos durante minha graduação, incluindo, resumos, provas, listas de exercícios resolvidas e etc.
+        </div>
       </header>
     </div>
   );
